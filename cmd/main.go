b02bd2e611639b9e4ec02192aa7fa871b22980c5
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-03-06 20:19:36 +0600

    Successfully product create and get

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"hisabi.com/m/config"
 	database "hisabi.com/m/databases"
+	"hisabi.com/m/internal/model"
+	"hisabi.com/m/routes"
 )
 
 func main() {
@@ -12,6 +15,10 @@ func main() {
 	config.Load()
 	database.Connect()
 
+	database.DB.AutoMigrate(&model.Product{})
+
+	mux := routes.SetUpRoutes()
+
 	log.Println("Server running on port 8080....")
-	//log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
