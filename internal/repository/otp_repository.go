c5d84186dc1e79583c41a7266d7b708fedf3901f
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-03-10 17:49:40 +0600

    user otp verify, mark used and user verified repo

--- internal/repository/otp_repository.go
+++ internal/repository/otp_repository.go
@@ -18,3 +18,33 @@ func SaveOTP(email, code string, expiresAt time.Time) error {
 	}
 	return database.DB.Create(otp).Error
 }
+
+// ------------------------------
+// Otp Search and Verify
+// ------------------------------
+func GetValidOTP(email, code string) (*model.OTP, error) {
+	var otp model.OTP
+	err := database.DB.Where(
+		"email = ? AND code = ? AND expires_at > ? AND is_used = false",
+		email, code, time.Now(),
+	).First(&otp).Error
+	return &otp, err
+
+}
+
+//------------------------------
+// OTP mark as used
+//------------------------------
+
+func MarkOTPUsed(id uint) error {
+	return database.DB.Model(&model.OTP{}).
+		Where("id = ?", id).
+		Update("is_used", true).Error
+}
+
+// ------------------------------
+// User verified mark
+// ------------------------------
+func MarkUserVerified(email string) error {
+	return database.DB.Model(&model.User{}).Where("email = ?", email).Update("is_verified", true).Error
+}
