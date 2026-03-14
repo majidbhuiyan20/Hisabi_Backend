1c91231e86a1ecd377b7644f9b5c1de2a04637e9
Majid Bhuiyan <65237569+majidbhuiyan20@users.noreply.github.com>
2026-03-14 10:26:13 +0600

    Refactor database connection string handling

--- databases/database.go
+++ databases/database.go
@@ -12,19 +12,26 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.Config.DBHost,
-		config.Config.DBPort,
-		config.Config.DBUser,
-		config.Config.DBPassword,
-		config.Config.DBName,
-	)
+	var dsn string
+
+	if config.Config.DatabaseURL != "" {
+		dsn = config.Config.DatabaseURL
+	} else {
+		
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+			config.Config.DBHost,
+			config.Config.DBPort,
+			config.Config.DBUser,
+			config.Config.DBPassword,
+			config.Config.DBName,
+		)
+	}
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Fatal("Database connection failed:", err)
 	}
 
 	log.Println("Database connected successfully")
